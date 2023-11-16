74e5efdaf599e542696e039e9f80b5a07848b17d
Your Name <you@example.com>
2023-11-16 01:07:07 -0300

    conectando com api

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,28 +1,43 @@
+export class GitHubUser {
+  static searche(username) {
+    const endPoint = `https://api.github.com/users/${username}`;
+
+    return fetch(endPoint)
+      .then((data) => data.json())
+      .then(({ login, name, public_repos, followers }) => ({
+        login,
+        name,
+        public_repos,
+        followers,
+      }));
+  }
+}
+
 // Classe que vai conter a logica de dados
 // Como os dados serao estruturados
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
     this.tbody = this.root.querySelector("table tbody");
-
     this.load();
+
+    GitHubUser.searche("felipe0402").then((user) => console.log(user));
   }
 
   load() {
-    this.entries = [
-      {
-        login: "felipe0402",
-        nome: "Felipe Andrade",
-        public_repos: "95",
-        followers: 20931,
-      },
-      {
-        login: "pablotelles",
-        nome: "Pablo Teles",
-        public_repos: "78",
-        followers: 2931,
-      },
-    ];
+    this.entries = JSON.parse(localStorage.getItem("github-favorites:")) || [];
+    console.log(this.entries);
+  }
+  async add(username) {
+    const user = await GitHubUser.searche(username);
+  }
+
+  delete(user) {
+    const filteRedRentries = this.entries.filter(
+      (entry) => entry.login !== user.login
+    );
+    this.entries = filteRedRentries;
+    this.update();
   }
 }
 
@@ -31,6 +46,16 @@ export class FavoritesView extends Favorites {
   constructor(root) {
     super(root);
     this.update();
+    this.onadd();
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector(".search button");
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector(".search input");
+
+      this.add(value);
+    };
   }
 
   update() {
@@ -47,6 +72,12 @@ export class FavoritesView extends Favorites {
       row.querySelector(".user span").textContent = user.login;
       row.querySelector(".repositories").textContent = user.public_repos;
       row.querySelector(".followers").textContent = user.followers;
+      row.querySelector(".remove").onclick = () => {
+        const isOk = confirm("Tem certeza que deseja deleta essa linha?");
+        if (isOk) {
+          this.delete(user);
+        }
+      };
 
       this.tbody.append(row);
 
